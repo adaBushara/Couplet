576f40d630413aa37dd12535b33fefe6dc05f4a3
Ada Bushara <bushara.ada@gmail.com>
2019-01-17 20:24:15 -0500

    [First Commit] [Added] Character Controller [Added] Main character to MainScene

---
+++ Couplet/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float moveSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+    //use this to handle our input
+    void HandleInput()
+    {
+        //let's get some keyboard input
+        //and rotate depending on the key that we are pressing
+
+        //left button, rotate left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, 1 * moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -1 * moveSpeed);
+        }
+
+
+    }
+
+
+    void Update () {
+        HandleInput();
+        
+	}
+
+    //our collision code
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision!");
+    }
+}
